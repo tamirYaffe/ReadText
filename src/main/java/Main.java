0b5cadb73b927debe6a5ef9ceff30c8c9d733751
tamiryaffe <tamiryaffe144@gmail.com>
2019-03-27 16:44:49 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+
+        //Loading an existing document
+        File file = new File("/Users/tamiryaffe/Desktop/part B q3 is q1.pdf");
+        PDDocument document = PDDocument.load(file);
+
+        //Instantiate PDFTextStripper class
+        PDFTextStripper pdfStripper = new PDFTextStripper();
+
+        //Retrieving text from PDF document
+        String text = pdfStripper.getText(document);
+        System.out.println(text);
+
+        //Closing the document
+        document.close();
+
+    }
+}
